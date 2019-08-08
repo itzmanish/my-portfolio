e8fec18ea09267e85baa0ad045981d8d2d404065
Manish <itzmanish108@gmail.com>
2019-08-08 21:36:52 +0530

    added font awesome icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,84 @@
 import React from "react"
-import { Link } from "gatsby"
+import Typed from "react-typed"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Layout from "../components/layout"
+import Header from "../components/Header"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <Header />
+    <div className="container">
+      <div className="layout">
+        <div className="main">
+          <h1 className="title">
+            Hi, I am <span className="highlight-text">Manish. </span> Full Stack
+            developer.
+          </h1>
+          <p>
+            I am proficient in {}
+            <span className="highlight-text">
+              <Typed
+                strings={[
+                  "React",
+                  "Gatsby",
+                  "Django",
+                  "Flask",
+                  "GoLang",
+                  "Python",
+                  "C",
+                  "C++",
+                ]}
+                typeSpeed={60}
+                backSpeed={50}
+                loop
+              />
+              .
+            </span>
+          </p>
+          <p style={{ marginTop: "20px" }}>
+            I also do system level programming. Some of languages that i use are
+            Python, Golang, C, C++. <br /> I do like reading books, listining
+            Music during work and travelling and learning new languages.
+            <br />
+            My interest are also in Robotics, IoT projects, Embeded programming
+            and electronics engineering.
+            <br />
+            Currently i am working on Machine learning using python.
+          </p>
+
+          <p>
+            Shoot a mail to{" "}
+            <a href="mailto:manish@izmanish.online">
+              <span className="highlight-text"> manish@izmanish.online </span>
+            </a>
+          </p>
+          <div className="bottom-nav">
+            <ul className="main-nav-list">
+              <li>
+                <a href="https://www.instagram.com/izmanish/" target="__blank">
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/itzmanish" target="__blank">
+                  <i class="fab fa-github"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/itzmanish"
+                  target="__blank"
+                >
+                  <i class="fab fa-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
