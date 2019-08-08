815366dca7feef4ecf742e03338a6d85d75c5007
Manish <itzmanish108@gmail.com>
2019-08-08 23:12:21 +0530

    little typo fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#fff`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icons/icon.png`, // This path is relative to the root of the site.
         // icons: [
         //   {
         //     src: `/favicons/android-chrome-192x192.png`,
