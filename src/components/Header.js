3d73e83e8f7111ca1c53704107fbb5061ec6cb78
Manish <itzmanish108@gmail.com>
2020-07-05 21:47:46 +0530

    Upgrade to v2 changelog- * Updated header social icons * Used more emojis * More concise and appropriate details * Some refactor on css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,43 @@
-import React from "react"
 import { Link } from "gatsby"
-import Logo from "../images/logo.svg";
+import React from "react"
+import Logo from "../images/logo.svg"
 
 const Header = () => {
-    return (
-        <header className="main-header">
-            <div className="container">
-                <Link to="/">
-                    <div className="logo">
-                        <Logo />
-                    </div>
-                </Link>
-                <nav className="main-nav m-hidden">
-                    <ul className="main-nav-list">
-                        <li>
-                            <a href="https://www.instagram.com/izmanish/" target="__blank">
-                                <i class="fab fa-instagram"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/itzmanish" target="__blank">
-                                <i class="fab fa-github"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.linkedin.com/in/itzmanish" target="__blank">
-                                <i class="fab fa-linkedin"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="mailto:manish@izmanish.online" target="__blank">
-                                <i class="fa fa-envelope"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header className="main-header">
+      <div className="container">
+        <Link to="/">
+          <div className="logo">
+            <Logo />
+          </div>
+        </Link>
+        <nav className="main-nav m-hidden">
+          <ul className="main-nav-list">
+            <li>
+              <a href="https://www.instagram.com/izmanish/" target="__blank">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com/itzmanish_/" target="__blank">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/itzmanish" target="__blank">
+                <i class="fab fa-github"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/itzmanish" target="__blank">
+                <i class="fab fa-linkedin"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,114 +1,145 @@
-import React, { useState, useEffect } from "react"
-import Typed from "react-typed"
-
-import SEO from "../components/seo"
-import Layout from "../components/layout"
+import React, { useEffect, useState } from "react"
 import Header from "../components/Header"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const IndexPage = () => {
+  const [dark, setDark] = useState(true)
+  const [toggleClass, setToggleClass] = useState("darkmode fas fa-sun")
 
-    const [dark, setDark] = useState(true)
-    const [toggleClass, setToggleClass] = useState("darkmode fas fa-sun")
+  useEffect(() => {
+    if (dark) {
+      document.body.classList.add("dark")
+      setToggleClass("darkmode fas fa-sun")
+    } else {
+      document.body.classList = ""
+      setToggleClass("darkmode fas fa-moon")
+    }
+  })
 
-    useEffect(()=>{
-        if(dark){
-            document.body.classList.add("dark")
-            setToggleClass("darkmode fas fa-sun")
-        }
-        else{
-            document.body.classList=""
-            setToggleClass("darkmode fas fa-moon")
-        }
-    })
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="overlay">
+        <Header />
+        <div className="container">
+          <div className="layout">
+            <div className="main">
+              <h1 className="title">
+                Hi{" "}
+                <span role="img" aria-label="hello">
+                  👋 <br />
+                </span>
+                I am <span className="highlight-text">Manish </span>
+                <br />
+                Full Stack developer.
+              </h1>
+              {/* <p>
+                I am proficient in {}
+                <span className="highlight-text">
+                  <Typed
+                    strings={[
+                      "ReactJS",
+                      "GatsbyJS",
+                      "Django",
+                      "Flask",
+                      "Go",
+                      "Python",
+                      "Flutter",
+                      "C",
+                      "C++",
+                    ]}
+                    typeSpeed={60}
+                    backSpeed={50}
+                    loop
+                  />
+                  .
+                </span>
+              </p> */}
+              <p style={{ marginTop: "40px" }}>
+                <span role="img" aria-label="computer">
+                  {" "}
+                  💻
+                </span>{" "}
+                Golang, Python, Javascript, Dart, C, Assembly Language.{" "}
+              </p>
+              <p>
+                <span role="img" aria-label="hearts">
+                  ❤️
+                </span>{" "}
+                Reading books, Travelling new places (more inclined to nature).
+              </p>
+              <p>
+                <span role="img" aria-label="corn">
+                  🌽
+                </span>{" "}
+                Django, Flask, ExpressJS, ReactJS, GatsbyJS, Flutter.{" "}
+              </p>
+              <p>
+                <span role="img" aria-label="triangular-rular">
+                  📐
+                </span>{" "}
+                Microservice architecture, Distributed systems design.
+              </p>
+              <p>
+                <span role="img" aria-label="dart">
+                  🎯
+                </span>{" "}
+                Internet Of Things, Embedded Programming, Artificial
+                Intellingence.
+              </p>
 
-    return (
-        <Layout>
-            <SEO title="Home" />
-            <div className="overlay">
-                <Header />
-                <div className="container">
-                    <div className="layout">
-                        <div className="main">
-                            <h1 className="title">
-                                Hi! <span role="img" aria-label="hello">👋 </span>
-
-                                I am <span className="highlight-text">Manish. </span>
-                                <br />
-                                Full Stack developer.
-          </h1>
-                            <p>
-                                I am proficient in {}
-                                <span className="highlight-text">
-                                    <Typed
-                                        strings={[
-                                            "React",
-                                            "Gatsby",
-                                            "Django",
-                                            "Flask",
-                                            "GoLang",
-                                            "Python",
-                                            "C",
-                                            "C++",
-                                        ]}
-                                        typeSpeed={60}
-                                        backSpeed={50}
-                                        loop
-                                    />
-                                    .
-            </span>
-                            </p>
-                            <p style={{ marginTop: "20px" }}>
-                                I also do system programming. Some of languages that i use are
-            Python, Golang, C, C++. <br /> I do like reading books, listining
-                                Music during work and travelling and learning new languages.
-            <br />
-                                My interest are also in Robotics, IoT projects, Embeded programming
-                                and electronics engineering.
-            <br />
-                                Currently i am working on Machine learning using python.
-          </p>
-
-                            <p>
-                                Shoot a mail to{" "}
-                                <a href="mailto:manish@izmanish.online">
-                                    <span className="highlight-text"> manish@izmanish.online </span>
-                                </a>
-                            </p>
-                            <div className="bottom-nav">
-                                <ul className="main-nav-list">
-                                    <li>
-                                        <a href="https://www.instagram.com/izmanish/" target="__blank">
-                                            <i class="fab fa-instagram"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://www.twitter.com/_itzmanish/" target="__blank">
-                                            <i class="fab fa-twitter"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://github.com/itzmanish" target="__blank">
-                                            <i class="fab fa-github"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="https://www.linkedin.com/in/itzmanish"
-                                            target="__blank"
-                                        >
-                                            <i class="fab fa-linkedin"></i>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <p style={{ marginTop: "40px" }}>
+                <span role="img" aria-label="email">
+                  📧
+                </span>
+                <a href="mailto:manish@izmanish.online">
+                  <span className="highlight-text">
+                    {" "}
+                    manish@izmanish.online
+                  </span>
+                </a>
+              </p>
+              <div className="bottom-nav">
+                <ul className="main-nav-list">
+                  <li>
+                    <a
+                      href="https://www.instagram.com/izmanish/"
+                      target="__blank"
+                    >
+                      <i class="fab fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.twitter.com/itzmanish_/"
+                      target="__blank"
+                    >
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://github.com/itzmanish" target="__blank">
+                      <i class="fab fa-github"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/itzmanish"
+                      target="__blank"
+                    >
+                      <i class="fab fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-           <div onClick={()=>setDark(!dark)} className={toggleClass}></div>
-
-        </Layout>
-    )
+          </div>
+        </div>
+      </div>
+      <div onClick={() => setDark(!dark)} className={toggleClass}></div>
+    </Layout>
+  )
 }
 
 export default IndexPage
